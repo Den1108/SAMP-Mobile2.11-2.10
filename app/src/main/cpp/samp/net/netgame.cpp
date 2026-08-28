88aac7df82049122fd772c8091212571428e2413
Den1 <zeromen945@gmail.com>
2026-08-28 07:53:14 +0000

    скрил дебаг сообщение

--- app/src/main/cpp/samp/net/netgame.cpp
+++ app/src/main/cpp/samp/net/netgame.cpp
@@ -488,7 +488,7 @@ void CNetGame::ProcessConnecting()
 {
 	if (GetTickCount() - m_dwLastConnectAttempt > 1000/*3000*/)
 	{
-		if (pUI) pUI->chat()->addDebugMessage("Connecting to %s:%d...", m_szHostOrIp, m_iPort);
+		//if (pUI) pUI->chat()->addDebugMessage("Connecting to %s:%d...", m_szHostOrIp, m_iPort);
 		if (pUI) pUI->chat()->addDebugMessage("Connecting to Server...");
 
 		m_pRakClient->Connect(m_szHostOrIp, m_iPort, 0, 0, 2);
